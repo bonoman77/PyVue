9911b359a8c508676ed34fe9bf1b136ca94fa8eb
김승균 <bonoman77@gmail.com>
2025-06-19 16:41:49 +0900

    정리

--- expense-tracker-vue/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import Toast from 'vue-toastification'
-import 'vue-toastification/dist/index.css'
-import './style.css'
-import App from './App.vue'
-
-const app = createApp(App).use(Toast)
-
-app.mount('#app')
--- todo-vue/todo_frontend/src/App.vue
+++ todo-vue/todo_frontend/src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { computed, ref } from 'vue'; 
+import { computed, ref, watch } from 'vue'; 
 import TodoForm from './components/TodoForm.vue'
 import TodoList from './components/TodoList.vue'
 import axios from 'axios'
@@ -7,22 +7,27 @@ import axios from 'axios'
 
 const todos = ref([])
 const error = ref('')
-const total_cnt = ref(0) 
-const row_size = 5 
-const page = ref(1)
+const totalCnt = ref(0) 
+const rowSize = 5 
+const currentPage = ref(1)
+const searchText = ref('')
+const numberOfPages = computed(() => {
+  return Math.ceil(totalCnt.value / rowSize)
+})
 
-const getTodos = async () => {
+const getTodos = async (page = currentPage.value) => {
+  currentPage.value = page
   try {
     const res = await axios.get('http://localhost:4000/boards/todo_list', {
       params: {
-        page: page.value,
-        row_size: row_size,
-        search_text: ''
+        page: currentPage.value,
+        row_size: rowSize,
+        search_text: searchText.value
       }
     })
     todos.value = res.data.todo_list
-    total_cnt.value = res.data.total_cnt
-    console.log(total_cnt.value)
+    totalCnt.value = res.data.total_cnt
+    console.log(totalCnt.value)
   } catch (err) {
     console.log(err)
     error.value = 'Failed to get todos' 
@@ -38,6 +43,7 @@ const addTodo = async (todo) => {
       completed: todo.completed,
       contents: todo.contents,
     })
+    getTodos(1); 
     todos.value.push(res.data)
   } catch (err) {
     console.log(err)
@@ -51,6 +57,7 @@ const deleteTodo = async (index) => {
     const todo_id = todos.value[index].todo_id
     await axios.delete('http://localhost:4000/boards/todo_delete/' + todo_id)
     todos.value.splice(index, 1)
+    getTodos()
   } catch (err) {
     console.log(err)
     error.value = 'Failed to delete todo' 
@@ -71,16 +78,26 @@ const toggleTodo = async (index) => {
   }
 }
 
-const searchText = ref('')
-
-const filteredTodos = computed(() => {
-  if (searchText.value){
-    return todos.value.filter((todo) => {
-      return todo.title.toLowerCase().includes(searchText.value.toLowerCase())
-    })
-  }
-  return todos.value
+let timeout = null; 
+const searchTodo = () => {
+  clearTimeout(timeout)
+  getTodos(1)
+}
+watch(searchText, () => {
+  clearTimeout(timeout)
+  timeout = setTimeout(() => {
+    getTodos(1)
+  }, 1000)
 })
+
+// const filteredTodos = computed(() => {
+//   if (searchText.value){
+//     return todos.value.filter((todo) => {
+//       return todo.title.toLowerCase().includes(searchText.value.toLowerCase())
+//     })
+//   }
+//   return todos.value
+// })
 
 </script>
 
@@ -92,21 +109,33 @@ const filteredTodos = computed(() => {
     <input type="text" 
     class="form-control mb-2"
     v-model="searchText" 
-    placeholder="Search todos...">
+    placeholder="Search todos..."
+    @keyup.enter="searchTodo">
     <TodoForm @add-todo="addTodo"/>
     <div class="text-danger">{{ error }}</div>
-    <div v-if="filteredTodos.length === 0" class="text-center py-2">
+    <div v-if="todos.length === 0" class="text-center py-2">
       No todos available.
     </div>
-    <TodoList :todos="filteredTodos" @delete-todo="deleteTodo" @toggle-todo="toggleTodo"/>
+    <TodoList :todos="todos" @delete-todo="deleteTodo" @toggle-todo="toggleTodo"/>
     <hr />
     <nav aria-label="Page navigation example">
       <ul class="pagination">
-        <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-        <li class="page-item"><a class="page-link" href="#">1</a></li>
-        <li class="page-item"><a class="page-link" href="#">2</a></li>
-        <li class="page-item"><a class="page-link" href="#">3</a></li>
-        <li class="page-item"><a class="page-link" href="#">Next</a></li>
+        <li v-if="currentPage > 1" class="page-item">
+          <a style="cursor: pointer;" class="page-link" @click="getTodos(currentPage - 1)">Previous</a>
+        </li>
+
+        <li v-for="page in numberOfPages" :key="page" 
+        class="page-item" 
+        :class="currentPage === page ? 'active' : ''"
+        @click="currentPage = page" 
+        >
+          <a style="cursor: pointer;" class="page-link" @click="getTodos(page)">
+            {{page}}
+          </a>
+        </li>
+        <li v-if="currentPage < numberOfPages" class="page-item">
+          <a style="cursor: pointer;" class="page-link" @click="getTodos(currentPage + 1)">Next</a>
+        </li>
       </ul>
     </nav>
   </div>
